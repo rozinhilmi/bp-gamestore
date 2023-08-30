45967df0dff6fa9cb030f2128bc017ba2160864c
= <=>
2023-08-30 15:20:51 +0700

    add modal product katalog

--- src/pages/Dashboard/sections/Products.tsx
+++ src/pages/Dashboard/sections/Products.tsx
@@ -1,16 +1,30 @@
-import { Button, HStack, Image, Stack, Text } from "@chakra-ui/react";
 import {
+  Button,
+  HStack,
+  Image,
+  Stack,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
+import {
+  primaryDarkColor,
   primaryTextDarkColor,
   secondaryDarkColor,
+  secondaryTextColor,
 } from "../../../utils/constant/theme";
 import {
   products,
   list_category_product,
 } from "../../../../public/database.json";
 import { useState } from "react";
+import { Modal, ModalOverlay, ModalContent, ModalBody } from "@chakra-ui/react";
+import { IoMdClose } from "react-icons/io";
 
 const Products = () => {
   const [selectedCategory, setSelectedCategory]: any = useState("");
+  const [selectedProduct, setSelectedProduct]: any = useState(null);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <Stack
       padding={{ base: "0px", lg: "30px" }}
@@ -70,6 +84,10 @@ const Products = () => {
         {products.map((item: any, index: number) =>
           item.category.includes(selectedCategory) ? (
             <Stack
+              onClick={() => {
+                setSelectedProduct(item);
+                onOpen();
+              }}
               key={index}
               width={{ base: "45%", sm: "170px", md: "220px" }}
               objectFit={"contain"}
@@ -85,6 +103,85 @@ const Products = () => {
           ) : null
         )}
       </HStack>
+
+      {selectedProduct ? (
+        <Modal
+          isOpen={isOpen}
+          onClose={() => {
+            setSelectedProduct(null);
+            onClose();
+          }}
+          size={"3xl"}
+        >
+          <ModalOverlay />
+          <ModalContent
+            backgroundColor={primaryDarkColor}
+            color={primaryTextDarkColor}
+            borderRadius={"12px"}
+            position={"relative"}
+          >
+            <ModalBody>
+              <Button
+                size={{ base: "xs", sm: "sm" }}
+                colorScheme="whiteAlpha"
+                position={"absolute"}
+                right={"5"}
+                onClick={() => {
+                  setSelectedProduct(null);
+                  onClose();
+                }}
+              >
+                <IoMdClose />
+              </Button>
+              <Stack gap={"20px"} paddingY={"10px"}>
+                <Stack textAlign="center" lineHeight={"5"}>
+                  <Text fontSize={"20px"} as="b">
+                    Katalog Harga Produk - {selectedProduct.title}
+                  </Text>
+                  <Text color={secondaryTextColor}>
+                    Nikmati harga lebih murah dengan menjadi member crown!
+                  </Text>
+                </Stack>
+
+                <HStack
+                  flexWrap={"wrap"}
+                  justifyContent={"center"}
+                  color={primaryTextDarkColor}
+                  lineHeight={"3"}
+                >
+                  {[1, 2, 3, 4, 5, 6, 7, 8].map((item: number) => (
+                    <HStack
+                      backgroundColor={"rgb(43, 54, 80)"}
+                      padding={"20px"}
+                      borderRadius={"12px"}
+                      width={{ base: "100%", md: "49%" }}
+                      alignItems={"center"}
+                      justifyContent={"space-between"}
+                      key={item}
+                    >
+                      <Text>60 + 6 Bonds</Text>
+                      <Stack fontSize={"14px"}>
+                        <HStack justifyContent={"flex-end"}>
+                          <Text>Gold</Text>
+                          <Text>Rp. 13.145,-</Text>
+                        </HStack>
+                        <HStack justifyContent={"flex-end"}>
+                          <Text>Crown</Text>
+                          <Text>Rp. 13.145,-</Text>
+                        </HStack>
+                        <HStack justifyContent={"flex-end"}>
+                          <Text>Platinum</Text>
+                          <Text>Rp. 13.145,-</Text>
+                        </HStack>
+                      </Stack>
+                    </HStack>
+                  ))}
+                </HStack>
+              </Stack>
+            </ModalBody>
+          </ModalContent>
+        </Modal>
+      ) : null}
     </Stack>
   );
 };
